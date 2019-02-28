26c665f8dc0dc909e38b663315cf85cde75af032
Mykola Shestopal <nick4eva@users.noreply.github.com>
2019-02-28 19:32:06 +0200

    Updated to use private IP address

--- zsh-ec2ssh.zsh
+++ zsh-ec2ssh.zsh
@@ -79,7 +79,7 @@ function zsh-ec2ssh() {
     fi
 
     echo "Fetching ec2 host..."
-    local selected_host=$(myaws ec2 ls --profile=${aws_profile_name} --region=${aws_region} --fields='InstanceId PublicIpAddress LaunchTime Tag:Name Tag:attached_asg' | sort -k4 | peco | cut -f2)
+    local selected_host=$(myaws ec2 ls --profile=${aws_profile_name} --region=${aws_region} --fields='InstanceId PublicIpAddress PrivateIpAddress LaunchTime Tag:Name' | sort -k5 | peco | cut -f3)
     if [ -n "${selected_host}" ]; then
         if [ -z "${proxy_host}" ]; then
             BUFFER="ssh -i ${target_private_key_path} -p ${target_port} ${target_user}@${selected_host} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
